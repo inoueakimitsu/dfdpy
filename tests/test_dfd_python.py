582ce6589acf88369de1190d4fe828b3fa642d1e
Akimitsu Inoue <akimitsu.inoue@gmail.com>
2022-09-10 20:55:41 +0000

    add buggy case

--- tests/test_dfd_python.py
+++ tests/test_dfd_python.py
@@ -6,11 +6,25 @@ from dfdpy.python import make_dfd, MermaidJsGraphExporter
 def test_make_dfd():
     
     script = textwrap.dedent("""\
-    a = hidden
-    b = a
-    c = b
-    d = c
-    """)
+a = b
+b = c
+if c == d:
+    e = f
+elif c == e:
+    e = g
+else:
+    if g == h:
+        h = i
+    else:
+        h = j
+    k = g
+def func(x):
+    s = x*2
+    ss = x**3
+    sss = s + ss
+    return sss
+k = k + func(a)
+""")
     hidden_id_list = [
         "hidden",
     ]
